28d62ca4e7f2428a53779a0a4a20ea5ab9e8488c
Sam Rijs <srijs@airpost.net>
2014-11-19 12:54:45 +1100

    refactor

--- app.go
+++ app.go
@@ -3,91 +3,100 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"io"
 	"log"
 	"net"
 	"net/http"
 )
 
-func proxy(preamble []byte, reader *bufio.Reader, conn *net.TCPConn, addr *net.TCPAddr) {
-	proxyConn, err := net.DialTCP("tcp", nil, addr)
-	if err != nil {
-		log.Print(err)
-		return
-	}
-	chLeft := make(chan []byte)
-	chRight := make(chan []byte)
-	chErr := make(chan error)
+func readChannel(r io.Reader, c io.Closer, chErr chan error) chan []byte {
+	chBuf := make(chan []byte)
 	go func() {
 		buf := make([]byte, 256)
 		for {
-			n, err := proxyConn.Read(buf)
+			n, err := r.Read(buf)
 			if err != nil {
 				chErr <- err
 				break
 			}
-			chRight <- buf[:n]
+			chBuf <- buf[:n]
 		}
-		conn.Close()
+		c.Close()
 	}()
-	go func() {
-		buf := make([]byte, 256)
+	return chBuf
+}
+
+func newProxy(addr *net.TCPAddr) func([]byte, *bufio.Reader, *net.TCPConn) {
+	return func(preamble []byte, reader *bufio.Reader, conn *net.TCPConn) {
+		proxyConn, err := net.DialTCP("tcp", nil, addr)
+		if err != nil {
+			log.Print(err)
+			return
+		}
+		chErr := make(chan error)
+		chRight := readChannel(proxyConn, conn, chErr)
+		chLeft := readChannel(reader, proxyConn, chErr)
+		preambleWritten := false
+		halfClosed := false
 		for {
-			n, err := reader.Read(buf)
-			if err != nil {
-				chErr <- err
-				break
-			}
-			chLeft <- buf[:n]
-		}
-		proxyConn.Close()
-	}()
-	preambleWritten := false
-	halfClosed := false
-	for {
-		select {
-		case data := <-chLeft:
-			if !preambleWritten {
-				proxyConn.Write(append(preamble, data...))
-				preambleWritten = true
-			} else {
-				proxyConn.Write(data)
-			}
-		case data := <-chRight:
-			conn.Write(data)
-		case <-chErr:
-			if !halfClosed {
-				halfClosed = true
-			} else {
-				return
-			}
-		default:
-			if !preambleWritten {
-				proxyConn.Write(preamble)
-				preambleWritten = true
+			select {
+			case data := <-chLeft:
+				if !preambleWritten {
+					proxyConn.Write(append(preamble, data...))
+					preambleWritten = true
+				} else {
+					proxyConn.Write(data)
+				}
+			case data := <-chRight:
+				conn.Write(data)
+			case <-chErr:
+				if !halfClosed {
+					halfClosed = true
+				} else {
+					return
+				}
+			default:
+				if !preambleWritten {
+					proxyConn.Write(preamble)
+					preambleWritten = true
+				}
 			}
 		}
 	}
 }
 
-func serve(conn *net.TCPConn) {
-	reader := bufio.NewReader(conn)
-	line, err := reader.ReadBytes('\n')
-	if err != nil {
-		log.Print(err)
-		return
+func serveProxy(listener *net.TCPListener) {
+	proxyBean := newProxy(&net.TCPAddr{[]byte{127, 0, 0, 1}, 11300, ""})
+	proxyHttp := newProxy(&net.TCPAddr{[]byte{127, 0, 0, 1}, 8000, ""})
+
+	for {
+		conn, err := listener.AcceptTCP()
+		if err != nil {
+			log.Fatal(err)
+		}
+		go func() {
+			reader := bufio.NewReader(conn)
+			line, err := reader.ReadBytes('\n')
+			if err != nil {
+				log.Print(err)
+				return
+			}
+			httpLineBuf := bytes.NewBuffer(line)
+			_, err = httpLineBuf.WriteString("\r\n\r\n")
+			if err != nil {
+				log.Print(err)
+				return
+			}
+			_, err = http.ReadRequest(bufio.NewReader(httpLineBuf))
+			if err != nil {
+				proxyBean(line, reader, conn)
+			} else {
+				proxyHttp(line, reader, conn)
+			}
+
+		}()
 	}
-	httpLineBuf := bytes.NewBuffer(line)
-	_, err = httpLineBuf.WriteString("\r\n\r\n")
-	if err != nil {
-		log.Print(err)
-		return
-	}
-	_, err = http.ReadRequest(bufio.NewReader(httpLineBuf))
-	if err != nil {
-		proxy(line, reader, conn, &net.TCPAddr{[]byte{127, 0, 0, 1}, 11300, ""})
-	} else {
-		proxy(line, reader, conn, &net.TCPAddr{[]byte{127, 0, 0, 1}, 8000, ""})
-	}
+
 }
 
 func main() {
@@ -99,23 +108,11 @@ func main() {
 	log.Println("Serving HTTP...")
 	go http.ListenAndServe("localhost:8000", nil)
 
-	_, err := net.ResolveTCPAddr("tcp", "localhost:8080")
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	listener, err := net.ListenTCP("tcp", &net.TCPAddr{[]byte{0, 0, 0, 0}, 8080, ""})
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	log.Println("Serving TCP...")
-	for {
-		conn, err := listener.AcceptTCP()
-		if err != nil {
-			log.Fatal(err)
-		}
-		go serve(conn)
-	}
+	serveProxy(listener)
 
 }
